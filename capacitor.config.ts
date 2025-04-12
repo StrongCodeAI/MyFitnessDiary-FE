a99233f20d2de2191563bbb7df7a6be597b40b07
Jose Daniel <jdsolano@zinkee.com>
2025-04-12 21:13:15 +0200

    Add Capacitor support and update dependencies
    - Added capacitor.config.ts for Capacitor configuration.
    - Updated .gitignore to exclude Android build files.
    - Included Capacitor dependencies in package.json and package-lock.json for Android support.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.strongCodeAi.com',
+  appName: 'my-fitness-diary',
+  webDir: 'dist/my-fitness-diary/browser'
+};
+
+export default config;
