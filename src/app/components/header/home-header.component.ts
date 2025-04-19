396fbe1812e12afae5c7092243ad6cf8b41b9f6e
Jose Daniel <josopu96@gmail.com>
2025-04-19 12:51:13 +0200

    Reemplaza el ícono de configuración en el encabezado por un componente personalizado, mejorando la modularidad y la consistencia visual. Se añaden las importaciones necesarias para el nuevo componente de ícono.

--- src/app/components/header/home-header.component.ts
+++ src/app/components/header/home-header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import User from '../../models/user.interface';
+import { SettingsIconComponent } from '../icons/settings-icon.component';
 
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.css'],
+  imports: [SettingsIconComponent]
 })
 export class HomeHeaderComponent {
   isHomePage: boolean = false;
